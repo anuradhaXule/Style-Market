bcdef25c2cf5c30b1a1063e96fa6a157d834717b
3LOGIXS\anuradha_k <anuradhaxule@gmail.com>
2018-12-04 11:32:36 +0530

    04/12/2018

--- dbServices/server.js
+++ dbServices/server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const app = express();
 
 const Stylist = require('../models').stylist;
+const Location = require('../models').location;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,6 +13,12 @@ app.use(bodyParser.json());
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-Stylist.findAll().then(users => console.log(users));
+app.get('/', (req,res) =>{
+    Stylist.findAll({
+        include:[Location]
+    }).then(stylists => {
+        res.render('')
+    });
+})
 
 app.listen(8080,()=>console.log('Listing to port 8080'));
